0effaf503c5e6cc279ac7db60fae6d7084a2bfc3
Sergiu Plesco <plescosergiu@gmail.com>
2026-01-31 06:48:41 -0500

    fix: rename name from prompt to username

--- number_guess.sh
+++ number_guess.sh
@@ -3,34 +3,33 @@
 PSQL="psql -X --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 random_number=$(( $RANDOM % 1000 + 1 ))
-# echo $random_number
+echo $random_number
 # shuffled_num=$(shuf -i 1-1000 -n 1)
 # echo $shuffled_num
 
 MAIIN_FUNCTION() {
   GUESS_COUNT=0
-  
-  while true; do
-    echo "Enter your name:"
-    read -r NAME
-    if (( ${#NAME} > 0 && ${#NAME} <=22 )); then
-      break
-    else
-      echo "Username bust be 1-22 characters."
-    fi
-  done
- 
-  USERNAME=$($PSQL "SELECT player_id FROM players WHERE player_name = '$NAME';")
 
-  if [[ -z $USERNAME ]]; then
-    echo -e "\nWelcome, $NAME! It looks like this is your first time here."
-    INSERT_NEW_PLAYER=$($PSQL "INSERT INTO players(player_name) VALUES('$NAME');")
-    PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE player_name = '$NAME';")
-  else
-    GAME=$($PSQL "SELECT p.player_id, p.player_name, COUNT(g.game_id) AS GAMES_PLAYED, MIN(g.guess_count) AS best_game FROM players p LEFT JOIN games g USING(player_id) WHERE g.player_id = $PLAYER_ID GROUP BY p.player_id, p.player_name")
-    IFS="|" read PLAYER_ID PLAYER_NAME GAMES_PLAYED BEST_GAME <<< "$GAME"
+  echo "Enter your username:"
+  read -r NAME
+  
  
-    echo -e "\nWelcome back, $PLAYER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."    
+ 
+  PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE username = '$NAME';")
+
+  if [[ -z $PLAYER_ID ]]; then
+    echo -e "\nWelcome, $NAME! It looks like this is your first time here."
+   
+    INSERT_NEW_PLAYER=$($PSQL "INSERT INTO players(username) VALUES('$NAME');")
+
+   
+    PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE username = '$NAME';")
+  else
+    USERNAME=$($PSQL "SELECT username FROM players WHERE username = '$NAME';")
+    GAME=$($PSQL "SELECT p.player_id, p.username, COUNT(g.game_id) AS GAMES_PLAYED, MIN(g.guess_count) AS best_game FROM players p LEFT JOIN games g USING(player_id) WHERE g.player_id = $PLAYER_ID GROUP BY p.player_id, p.username")
+    IFS="|" read PLAYER_ID USERNAME GAMES_PLAYED BEST_GAME <<< "$GAME"
+ 
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."    
   fi 
 
   echo -e "\nGuess the secret number between 1 and 1000:"
